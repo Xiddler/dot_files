0c7ab5a5636190bfac649cae297c461f08161fea
Donagh <xiddler@gmail.com>
2018-04-18 21:30:11 +0100

    2018-04-18_21:28

--- info.sh
+++ info.sh
@@ -1,6 +1,6 @@
 # check some system items when using Live USB
 
-echo "Memory usage is :" && df -h | grep aufs | awk '{print $5}'
-echo "CPU is currently used by :" && ps aux | awk '$3+$4> 5' | awk '{print $11 "  " $3+$4}'
+echo "Memory usage is :" df -h | grep aufs | awk '{print $5}'
+echo "CPU is currently used by :" &&  ps aux | awk '$3+$4> 5' | awk '{print $11 "  " $3+$4}'
 
 
